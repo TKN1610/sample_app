7ec985f5a36c67facdaff77e2c7e6b58efff3cf7
TKN1610 <tkn1610@gmail.com>
2025-02-23 19:38:51 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,26 +2,21 @@ require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup information" do
+  test "valid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_response :unprocessable_entity
-    assert_template 'users/new'
     
-    # エラーメッセージの表示確認
-    assert_select 'div#error_explanation'  # エラーを囲むdivのid
-    assert_select 'div.field_with_errors'  # フォームのエラークラス
-
-    # 特定のエラーメッセージが含まれるか
-    assert_select 'div#error_explanation', /Name can't be blank/
-    assert_select 'div#error_explanation', /Email is invalid/
-    assert_select 'div#error_explanation', /Password is too short/
+    # ✅ flashメッセージが表示されるか確認
+    assert_not flash.empty?  # flashにメッセージが入っているか
+    
+    # ✅ flashメッセージが表示されるHTML要素があるか確認
+    assert_select 'div.alert-success', "Welcome to the Sample App!"
   end
 end
